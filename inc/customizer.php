521d955bcb7ad83fa29a0080a8141f3d390327f5
Aleksandr <algryshko@gmail.com>
2023-08-29 16:33:04 +0300

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -3,7 +3,7 @@
 // Add custom fields to Site Identity Tab
 function custom_theme_customizer( $wp_customize ) {
   // Modify the Site Identity section
-  $wp_customize->get_section( 'title_tagline' )->title = __( 'Site Identity', 'custom-theme' );
+  $wp_customize->get_section( 'title_tagline' )->title = __( 'Site Identity', 'emptytheme' );
 
   // Add an image field for Logo
   $wp_customize->add_setting( 'logo_setting', array(
@@ -11,7 +11,7 @@ function custom_theme_customizer( $wp_customize ) {
   ) );
 
   $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo_control', array(
-      'label' => __( 'Logo', 'custom-theme' ),
+      'label' => __( 'Logo', 'emptytheme' ),
       'section' => 'title_tagline',
       'settings' => 'logo_setting',
   ) ) );
@@ -33,3 +33,69 @@ function theme_logo() {
 
   return $template;
 }
+
+// Add custom fields to Social Media Links Tab
+function social_theme_customizer( $wp_customize ) {
+  $wp_customize->add_section( 'social_media_section', array(
+    'title' => __( 'Social Media Links', 'emptytheme' ),
+    'priority' => 30,
+  ) );
+
+  // Whatsup Field
+  $wp_customize->add_setting( 'whatsup_url_setting', array(
+      'default' => '',
+  ) );
+
+  $wp_customize->add_control( 'whatsup_url_control', array(
+      'label' => __( 'Whatsup URL', 'emptytheme' ),
+      'section' => 'social_media_section',
+      'settings' => 'whatsup_url_setting',
+      'type' => 'url',
+  ) );
+
+  // Telegram Field
+  $wp_customize->add_setting( 'telegram_url_setting', array(
+      'default' => '',
+  ) );
+
+  $wp_customize->add_control( 'telegram_url_control', array(
+      'label' => __( 'Telegram URL', 'emptytheme' ),
+      'section' => 'social_media_section',
+      'settings' => 'telegram_url_setting',
+      'type' => 'url',
+  ) );
+
+  // LinkedIn Field
+  $wp_customize->add_setting( 'linkedin_url_setting', array(
+      'default' => '',
+  ) );
+
+  $wp_customize->add_control( 'linkedin_url_control', array(
+      'label' => __( 'LinkedIn URL', 'emptytheme' ),
+      'section' => 'social_media_section',
+      'settings' => 'linkedin_url_setting',
+      'type' => 'url',
+  ) );
+}
+add_action( 'customize_register', 'social_theme_customizer' );
+
+// Add custom fields to Copyrights Tab
+function copyrights_theme_customizer( $wp_customize ) {
+  $wp_customize->add_section( 'copyrights_section', array(
+    'title' => __( 'Copyrights', 'emptytheme' ),
+    'priority' => 100,
+  ) );
+
+  // Copyrights Field
+  $wp_customize->add_setting( 'copyrights_text_setting', array(
+      'default' => '',
+  ) );
+
+  $wp_customize->add_control( 'copyrights_text_control', array(
+      'label' => __( 'Copyrights Text', 'emptytheme' ),
+      'section' => 'copyrights_section',
+      'settings' => 'copyrights_text_setting',
+      'type' => 'url',
+  ) );
+}
+add_action( 'customize_register', 'copyrights_theme_customizer' );
